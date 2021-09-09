8a140e05316efbee0acedb183328aca7266a9b8d
Ben Olson <regnare@gmail.com>
2021-09-09 10:36:42 -0400

    Update remote path

--- teleporter.sh
+++ teleporter.sh
@@ -6,4 +6,4 @@
 # Copy files to remote location
 /usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 \
   --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s \
-  /home/ben/pihole/teleporter pihole:backups/pihole/
+  /home/ben/pihole/teleporter pihole:rclone/pihole/
