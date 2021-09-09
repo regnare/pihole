d92d60733483bf3c203593f61007ed8adf32ab4f
Ben Olson <regnare@gmail.com>
2021-09-09 10:19:05 -0400

    Update teleporter.sh to include remote backup

--- teleporter.sh
+++ teleporter.sh
@@ -2,3 +2,8 @@
 # Script to backup the pihole config within the docker container.
 
 /usr/bin/docker exec -w "/teleporter" pihole pihole -a -t
+
+# Copy files to remote location
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 \
+  --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s \
+  /home/ben/pihole/teleporter pihole:backups/pihole/
